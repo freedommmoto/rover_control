06ea2c1ca42105d2790bf3db2f9aa05747b6999b
Patara.m <patara.m@cartrack.com>
2022-08-13 14:50:28 +0700

    add FormatStingSliceToRoverCommand function with check mapsize and rover command.

--- tool/formatOutPut.go
+++ tool/formatOutPut.go
@@ -1,6 +1,7 @@
 package tool
 
 import (
+	"errors"
 	"strconv"
 )
 
@@ -8,4 +9,27 @@ func FormatPositionRover(direction string, positionX int, positionY int) string 
 	positionXStr := strconv.Itoa(positionX)
 	positionYStr := strconv.Itoa(positionY)
 	return direction + ":" + positionXStr + "," + positionYStr
+}
+
+func FormatStingSliceToRoverCommand(str []string) (mapSize int, route []string, err error) {
+	allLine := make([]string, 0)
+
+	for i, strLine := range str {
+		if i == 0 {
+			mapSize, err = strconv.Atoi(strLine)
+			if mapSize < 1 {
+				return 0, allLine, errors.New("first line is not map size")
+			}
+			if err != nil {
+				return 0, allLine, err
+			}
+		} else {
+			if len(strLine) == 1 {
+				allLine = append(allLine, strLine)
+			} else {
+				return 0, nil, errors.New("command not long 1 character")
+			}
+		}
+	}
+	return mapSize, allLine, nil
 }
--- tool/formatOutPut_test.go
+++ tool/formatOutPut_test.go
@@ -5,8 +5,46 @@ import (
 	"testing"
 )
 
+var SliceTestOkCase = []string{
+	"10",
+	"F",
+	"R",
+	"F",
+	"L",
+	"F",
+}
+
+var SliceTestFailCaseCommandNot1Carlong = []string{
+	"10",
+	"forward",
+	"R",
+}
+
+var SliceTestFailMapSizeNotInt = []string{
+	"R",
+	"12",
+	"F",
+}
+
 func TestFormatPositionRover(t *testing.T) {
 	str := FormatPositionRover("N", 1, 1)
 	require.NotEmpty(t, str)
 	require.Equal(t, str, "N:1,1")
+}
+
+func TestFormatStingSliceToRoverCommand(t *testing.T) {
+	mapSize, roverCommand, err := FormatStingSliceToRoverCommand(SliceTestOkCase)
+	roverCommandTest := SliceTestOkCase[1:6]
+
+	require.NotEmpty(t, mapSize)
+	require.NotEmpty(t, roverCommand)
+	require.NoError(t, err)
+	require.Equal(t, roverCommand, roverCommandTest)
+
+	//case fail
+	_, _, err = FormatStingSliceToRoverCommand(SliceTestFailCaseCommandNot1Carlong)
+	require.Error(t, err)
+
+	_, _, err = FormatStingSliceToRoverCommand(SliceTestFailMapSizeNotInt)
+	require.Error(t, err)
 }
